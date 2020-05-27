23aa0b9e91464dc8a2d23001665aba92d1dad49d
jooyae <jooyae3970@naver.com>
2020-05-28 02:35:11 +0900

    guideline & constraintdimensionratio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testsopt.R
 
 class MainActivity : AppCompatActivity() {
 
